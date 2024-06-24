1d02fff18b2e4ef0ba4b9f9cf55c07d34337c11a
DESKTOP-4013JRM\Nagunt <reiy0902@naver.com>
2024-06-24 23:20:21 +0900

    구조 초안 작성

---
+++ Assets/1_Scripts/KSH/Order.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MarsDonalds
+{
+    public class Order
+    {
+        public int id;
+        public float orderTime;
+
+        public bool isComplete;
+    }
+}
---
+++ Assets/1_Scripts/KSH/Stage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MarsDonalds
+{
+    public class Stage
+    {
+        public int date;
+        public float timeLimit;
+    }
+}

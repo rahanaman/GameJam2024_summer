ebb4e4609af490254fa5553fea0eefa7bc9092b0
DESKTOP-4013JRM\Nagunt <reiy0902@naver.com>
2024-06-25 19:17:58 +0900

    Fill 오타 수정.

--- Assets/1_Scripts/CookData.cs
+++ Assets/1_Scripts/CookData.cs
@@ -9,7 +9,7 @@ namespace MarsDonalds
     {
         
         public int PotatoID { get; private set; } // 0,1,2,3 //°¨ÀÚ, ÂüÄ¡¸À, °í±¸¸¶¸À, °è¶õ¸À
-        public int PeelState { get; private set; } //0,1
+        public int PillState { get; private set; } //0,1
         public int CutState { get; private set; } // 0,1, 2,3 -  x, Ã¤½ä±â,±ïµÏ½ä±â, Ÿ‡Áö½ä±â
 
         public List<int> CookState { get; private set; } //0,1,2,3,4 
@@ -19,7 +19,7 @@ namespace MarsDonalds
         public CookData(int id)
         {
             PotatoID = id;
-            PeelState = 0;
+            PillState = 0;
             CutState = 0;
             CookState = new List<int>() {0};
         }

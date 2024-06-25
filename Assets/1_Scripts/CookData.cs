53be424df6e9508880dc0e7fe8adee1bf4d55ee9
Aman <gr0315@cau.ac.kr>
2024-06-25 16:05:41 +0900

    CookData

--- Assets/1_Scripts/CookData.cs
+++ Assets/1_Scripts/CookData.cs
@@ -10,5 +10,16 @@ namespace MarsDonalds
         public int PotatoID { get; private set; } // 0,1,2,3 //°¨ÀÚ, ÂüÄ¡¸À, °í±¸¸¶¸À, °è¶õ¸À
         public int PeelState { get; private set; } //0,1
         public int CutState { get; private set; } // 0,1, 2,3 -  x, Ã¤½ä±â,±ïµÏ½ä±â, Ÿ‡Áö½ä±â
+
+        public List<int> CookState { get; private set; } //0,1,2,3,4 
+
+
+        public CookData(int id)
+        {
+            PotatoID = id;
+            PeelState = 0;
+            CutState = 0;
+            CookState = new List<int>() { 0};
+        }
     }
 }
